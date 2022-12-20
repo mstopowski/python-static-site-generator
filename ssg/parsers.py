3643b3070cf101db9f8db9277fd697b438bcadf1
mstopowski <49496093+mstopowski@users.noreply.github.com>
2022-12-20 18:08:55 +0100

    End of module 5

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -1,3 +1,8 @@
+import sys
+from docutils.core import publish_parts
+from markdown import markdown
+from ssg.content import Content
+
 from typing import List
 from pathlib import Path
 import shutil
@@ -30,3 +35,24 @@ class ResourceParser(Parser):
 
     def parse(self, path: Path, source: Path, dest: Path):
         super().copy(path, source, dest)
+
+
+class MarkdownParser(Parser):
+    extensions = [".md", ".markdown"]
+
+    def parse(self, path: Path, source: Path, dest: Path):
+        content = Content.load(self.read(path))
+        html = markdown(content.body)
+        self.write(path, dest, html)
+        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
+
+
+class ReStructuredTextParser(Parser):
+    extensions = [".rst"]
+
+    def parse(self, path: Path, source: Path, dest: Path):
+        content = Content.load(self.read(path))
+        publish_parts(content.body)
+        html = publish_parts(content.body, writer_name="html5")
+        self.write(path, dest, html["html_body"])
+        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
